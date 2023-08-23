0db658b737202c46449aec287ef7c7592b56982f
Azat <azat@garaev.ru>
2023-08-23 14:28:02 +0300

    AllTasksAreDone

---
+++ AutoTests/PageObjects/YandexHome.py
@@ -0,0 +1,65 @@
+import json
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class YandexHome:
+    def __init__(self, driver, avoidCaptchaWithCookies=False):
+        self.driver = driver
+        self.driver.get("https://ya.ru/")
+        self.search_input_xpath = '//input[@id="text"]'
+        self.services_menu_xpath = '/html/body/main/div[@class="body__content"]/form//div[@class="services-suggest__icons-more"]'
+        self.images_service_xpath = '/html/body/main/div[4]/div/div[1]/div/div[3]/div[1]/span[9]/a[@aria-label="Картинки"]'
+        self.suggest_table_xpath = '//ul[@class="mini-suggest__popup-content"]'
+
+        if avoidCaptchaWithCookies:
+            with open('ya_cookies.json', 'r') as f:
+                cookies = json.load(f)
+            for i in cookies:
+                driver.add_cookie(i)
+            driver.refresh()
+
+    def search_typing(self, query):
+        search_input = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, self.search_input_xpath))
+        )
+        search_input.send_keys(query)
+
+    def search_field_click(self):
+        search_input = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, self.search_input_xpath))
+        )
+        search_input.click()
+
+    def get_search_field(self):
+        return WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, self.search_input_xpath))
+        )
+
+    def get_suggest_table(self):
+        return WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, self.suggest_table_xpath))
+        )
+
+    def search_enter(self):
+        search_input = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, self.search_input_xpath))
+        )
+        search_input.send_keys(Keys.ENTER)
+
+    def get_services_menu(self):
+        return WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, self.services_menu_xpath))
+        )
+
+    def open_image_service(self):
+        services_menu = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, self.services_menu_xpath))
+        )
+        services_menu.click()
+        images_service = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, self.images_service_xpath))
+        )
+        images_service.click()
---
+++ AutoTests/PageObjects/YandexImages.py
@@ -0,0 +1,72 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class YandexImageCategories:
+    def __init__(self, driver):
+        self.driver = driver
+        self.list_of_categories_xpath = '//*[@class="PopularRequestList"]/*'
+
+    def get_list_of_categories(self):
+        return WebDriverWait(self.driver, 10).until(EC.visibility_of_any_elements_located(
+            (By.XPATH, self.list_of_categories_xpath))
+        )
+
+    def choose_category_by_id(self, Id):
+        list_of_categories = WebDriverWait(self.driver, 10).until(EC.visibility_of_any_elements_located(
+            (By.XPATH, self.list_of_categories_xpath))
+        )
+        list_of_categories[Id].click()
+
+
+class YandexImages:
+    def __init__(self, driver):
+        self.driver = driver
+        self.list_of_images_xpath = '/html/body//div[@role="list"]/div[not(contains(@class, "incut"))]'
+        self.search_field_xpath = '//input[@class="input__control mini-suggest__input"]'
+
+    def get_search_field(self):
+        return WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(
+            (By.XPATH, self.search_field_xpath))
+        )
+
+    def choose_image_by_id(self, Id):
+        list_of_images = WebDriverWait(self.driver, 10).until(EC.visibility_of_any_elements_located(
+            (By.XPATH, self.list_of_images_xpath))
+        )
+        list_of_images[Id].click()
+
+    def get_list_of_images(self):
+        return WebDriverWait(self.driver, 10).until(EC.visibility_of_any_elements_located(
+            (By.XPATH, self.list_of_images_xpath))
+        )
+
+
+class YandexImageViewer:
+    def __init__(self, driver):
+        self.driver = driver
+        self.image_viewer_xpath = '/html/body//div[@class="MMImageContainer"]/img'
+        self.next_image_button_xpath = '/html/body//div[contains(@class, "CircleButton_type_next")]'
+        self.previous_image_button_xpath = '/html/body//div[contains(@class, "CircleButton_type_prev")]'
+
+    def get_current_image(self):
+        return WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(
+            (By.XPATH, self.image_viewer_xpath))
+        )
+
+    def next_image(self):
+        next_image_button = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(
+            (By.XPATH, self.next_image_button_xpath))
+        )
+        next_image_button.click()
+
+    def previous_image(self):
+        previous_image_button = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(
+            (By.XPATH, self.previous_image_button_xpath))
+        )
+        previous_image_button.click()
+
+
+
+
